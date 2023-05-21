3daf101bb5d27bed946c13ad08f1f5f63ce98901
ShevkoMore <shevkomore@gmail.com>
2023-05-21 13:16:52 +0300

    used cbrt for cube root

--- src/app/created-tasks/lw1/lw1-task1/lw1-task1.component.ts
+++ src/app/created-tasks/lw1/lw1-task1/lw1-task1.component.ts
@@ -37,7 +37,9 @@ export class Lw1Task1Component extends Task<Labwork1> {
     else if (positives > negatives){
       event.source.fillTextRepeatingMiddle('(','*',')<sup><sup>1</sup>&frasl;<sub>'+nums.length+'</sub></sup>') // <- writes "(#*#*#)^1/3", but with formatting
       // multiply all of them, left to right, then take lengthth root
-      this.result = Math.pow(nums.reduce((sum, next) => sum*=next), 1/nums.length)
+      if(nums.length == 3)
+        this.result = Math.cbrt(nums.reduce((sum, next) => sum*=next))
+      else this.result = Math.pow(nums.reduce((sum, next) => sum*=next), 1/nums.length)
     }
     else if (negatives > positives){
       event.source.fillTextRepeatingMiddle('(','+',')/'+nums.length)
